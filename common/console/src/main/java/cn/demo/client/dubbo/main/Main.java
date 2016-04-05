720120558f1c22688a208ca8e1920b33082ce408
laurusCN <keyed.cn@gmail.com>
2016-04-05 16:04:19 +0800

    协议客户端demo

--- common/console/src/main/java/cn/demo/client/dubbo/main/Main.java
+++ common/console/src/main/java/cn/demo/client/dubbo/main/Main.java
@@ -3,11 +3,11 @@
  * 
  * File Created at 2016年04月05日
  * 
- * Copyright 2016 naitang.com All right reserved. This software is the
- * confidential and proprietary information of naitang.com ("Confidential
+ * Copyright 2016 demo.com All right reserved. This software is the
+ * confidential and proprietary information of demo.com ("Confidential
  * Information"). You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered
- * into with naitang.com .
+ * into with demo.com .
  */
 package cn.demo.client.dubbo.main;
 
@@ -17,11 +17,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 /**
- * Created by TianMing <tianming@naitnag.com> on 16/4/5.
+ * Created by TianMing <tianming@demo.com> on 16/4/5.
  *
  * @Type Main
  * @Desc 普通类型转换
- * @Author TianMing <tianming@naitang.com>
+ * @Author TianMing <tianming@demo.com>
  * @Date 2016-04-05
  * @Version V1.0
  */
