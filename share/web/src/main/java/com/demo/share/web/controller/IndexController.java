e2e58575713607da2ff4716a582af7c87082c3da
laurusCN <keyed.cn@gmail.com>
2016-04-08 09:46:26 +0800

    update pom configure

--- share/web/src/main/java/com/demo/share/web/controller/IndexController.java
+++ share/web/src/main/java/com/demo/share/web/controller/IndexController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping("/one")
 public class IndexController {
+
 
     @RequestMapping("/showView")
     @ResponseBody()
@@ -16,6 +16,7 @@ public class IndexController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("viewName");
         modelAndView.addObject(" 需要放到 model 中的属性名称 ", " 对应的属性值，它是一个对象 ");
+
         return modelAndView;
     }
 }
